c729a713df0d864d0a71e10674f15aa84a7a0560
supersathit <tatansathit.w@gmail.com>
2025-02-11 11:50:01 +0700

    Create modal.php

---
+++ components/modal.php
@@ -0,0 +1,41 @@
+<!-- Modal check m4-->
+<div class="modal fade" id="staticBackdropM4" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabelM4" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-warning">
+                <h1 class="modal-title fs-5" id="staticBackdropLabelM4">ชั้นมัธยมศึกษาปีที่ 4</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="" method="post">
+            <div class="modal-body">
+                <p class="text-danger">ตรวจสอบเลขบัตรประจำตัวประชาชน</p>
+                <input type="text" name="" id="" class="form-control" placeholder="กรอกเลขบัตรประชาชน 13 หลัก">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" data-bs-dismiss="modal">ตกลง</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Modal check m1-->
+<div class="modal fade" id="staticBackdropM1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabelM1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-warning">
+                <h1 class="modal-title fs-5" id="staticBackdropLabelM1">ชั้นมัธยมศึกษาปีที่ 1</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="" method="post">
+            <div class="modal-body">
+                <p class="text-danger">ตรวจสอบเลขบัตรประจำตัวประชาชน</p>
+                <input type="text" name="" id="" class="form-control" placeholder="กรอกเลขบัตรประชาชน 13 หลัก">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" data-bs-dismiss="modal">ตกลง</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
