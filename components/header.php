e2603d6db90d60e79cb96fae1b074c8048f96273
supersathit <tatansathit.w@gmail.com>
2025-02-11 10:50:02 +0700

    Update header.php

--- components/header.php
+++ components/header.php
@@ -1,4 +1,4 @@
-<div class="header shadow mb-5">
+<div class="header shadow mb-4">
     <img src="img/logo โรงเรยีน.png" height="40px">
     <div class="text-header" onclick="window.location.assign('index.php')">ระบบรับนักเรียน ปีการศึกษา 2568 โรงเรียนวัชรวิทยา</div>
 </div>
