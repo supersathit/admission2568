141cf5597f91e3b2f733d61dc3b360a6387132ea
supersathit <tatansathit.w@gmail.com>
2025-03-18 12:51:46 +0700

    Update dblink.php

--- dblink.php
+++ dblink.php
@@ -1,6 +1,6 @@
 <?php 
 $conn = mysqli_connect("localhost","root","","admission") or die ("Error :".mysqli_connect_error($conn));
 mysqli_query($conn,"SET NAMES 'utf8'");
-error_reporting( error_reporting() & ~E_NOTICE );
-date_default_timezone_set('Asia/Bangkok');  
+// error_reporting( error_reporting() & ~E_NOTICE );
+date_default_timezone_set('Asia/Bangkok');
 ?>
