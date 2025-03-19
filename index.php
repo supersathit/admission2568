82a5853337d7c3780a5675d0fa30829b89325894
supersathit <tatansathit.w@gmail.com>
2025-03-19 15:35:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
         <p class="text-center text-danger">เพื่อความสะดวก ท่านสามารถติดต่อได้ทาง Line : <a href="https://page.line.me/145xeekj" class="btn btn-success"><i class="fa-regular fa-hand-pointer"></i> <i class="fa-brands fa-line"></i> WR-Admission2568</a></p>
       </div>
 
+
+      <!-- สมัครห้องเรียนพิเศษ -->
       <!-- <div class="row justify-content-center mb-3">
 
         <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
@@ -55,11 +57,54 @@
 
       </div> -->
 
+      <!-- สมัครห้องเรียนปกติ -->
+      <div class="row justify-content-center mb-3">
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+          <div class="card shadow rounded-4 p-2">
+            <h4 class="fw-bold mt-2 text-center">ชั้นมัธยมศึกษาปีที่ 1</h4>
+            <p class="text-center">ประเภท : ห้องเรียนปกติ</p>
+            <button type="button" class="btn btn-lg btn-success" data-bs-toggle="modal" id="openModalM1" data-bs-target="#staticBackdropM1">
+              <i class="fa-solid fa-right-to-bracket fs-2"></i><br> ต้องการสมัคร <br>ชั้นมัธยมศึกษาปีที่ 1
+            </button>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+          <div class="card shadow rounded-4 p-2">
+            <h4 class="fw-bold mt-2 text-center">ชั้นมัธยมศึกษาปีที่ 4</h4>
+            <p class="text-center">ประเภท : ห้องเรียนปกติ</p>
+            <button type="button" class="btn btn-lg btn-info" data-bs-toggle="modal" id="openModalM4" data-bs-target="#staticBackdropM4">
+              <i class="fa-solid fa-right-to-bracket fs-2"></i><br> ต้องการสมัคร <br>ชั้นมัธยมศึกษาปีที่ 4
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mb-3">
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+          <div class="card shadow rounded-4 p-2">
+            <h4 class="fw-bold mt-2 text-center">ยืนยันสิทธิ์<br>ชั้นมัธยมศึกษาปีที่ 1</h4>
+            <p class="text-center">ประเภท : ห้องเรียนปกติ</p>
+            <button type="button" class="btn btn-lg btn-warning" data-bs-toggle="modal" id="openModalM1" data-bs-target="#ModalOpen">
+              <i class="fa-solid fa-right-to-bracket fs-2"></i><br> ยืนยันสิทธิ์สำหรับคนที่เคยสมัครห้องเรียนพิเศษแล้ว<br>ชั้นมัธยมศึกษาปีที่ 1
+            </button>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+          <div class="card shadow rounded-4 p-2">
+            <h4 class="fw-bold mt-2 text-center">ยืนยันสิทธิ์<br>ชั้นมัธยมศึกษาปีที่ 4</h4>
+            <p class="text-center">ประเภท : ห้องเรียนปกติ</p>
+            <button type="button" class="btn btn-lg btn-primary" data-bs-toggle="modal" id="openModalM4" data-bs-target="#ModalOpen">
+              <i class="fa-solid fa-right-to-bracket fs-2"></i><br> ยืนยันสิทธิ์สำหรับคนที่เคยสมัครห้องเรียนพิเศษแล้ว<br>ชั้นมัธยมศึกษาปีที่ 4
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div id="status" class="row justify-content-center mb-2">
         <div class="col-lg-8 col-md-12 col-sm-12 col-12 mb-3">
           <div class="card shadow rounded-4 p-3">
-            <h4 class="fw-bold text-center">ดูห้องสอบและเลขที่นั่งสอบ ม.1</h4>
-            <p class="fw-bold text-center">สามารถตรวจสอบห้องสอบและเลขที่นั่งสอบโดยการกรอกเลขประจำตัวประชาชน</p>
+            <h4 class="fw-bold text-center">ตรวจสอบสถานะการสมัคร ม.1</h4>
+            <p class="fw-bold text-center">สามารถตรวจสอบสถานะการสมัครห้องเรียนปกติโดยการกรอกเลขประจำตัวประชาชน</p>
             <form action="exam_id_m1.php" method="get" class="p-3 bg-light">
               <div class="input-group">
                 <input type="text" name="code_id" class="form-control form-control-lg" placeholder="กรอกเลขบัตรประชาชนที่สมัครแล้ว" maxlength="13" OnKeyPress="return chkNumber(this)" aria-label="Example text with button addon" aria-describedby="button-addon1">
@@ -72,8 +117,8 @@
       <div class="row justify-content-center mb-5">
         <div class="col-lg-8 col-md-12 col-sm-12 col-12 mb-3">
           <div class="card shadow rounded-4 p-3">
-            <h4 class="fw-bold text-center">ดูห้องสอบและเลขที่นั่งสอบ ม.4</h4>
-            <p class="fw-bold text-center">สามารถตรวจสอบห้องสอบและเลขที่นั่งสอบโดยการกรอกเลขประจำตัวประชาชน</p>
+            <h4 class="fw-bold text-center">ตรวจสอบสถานะการสมัคร ม.4</h4>
+            <p class="fw-bold text-center">สามารถตรวจสอบสถานะการสมัครห้องเรียนปกติโดยการกรอกเลขประจำตัวประชาชน</p>
             <form action="exam_id_m4.php" method="get" class="p-3 bg-light">
               <div class="input-group">
                 <input type="text" name="code_id" class="form-control form-control-lg" placeholder="กรอกเลขบัตรประชาชนที่สมัครแล้ว" maxlength="13" OnKeyPress="return chkNumber(this)" aria-label="Example text with button addon" aria-describedby="button-addon1">
@@ -154,22 +199,22 @@
           $value = mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(*) FROM regis_m1 WHERE date_regis <= '$date1' AND date_regis > '$date2' "));
           return $value['COUNT(*)'];
         }
-        $dayM1_1 = date_time_m1("2025-02-15 16:30:00","2025-02-13 16:30:00");
-        $dayM1_2 = date_time_m1("2025-02-16 16:30:00","2025-02-15 16:30:00");
-        $dayM1_3 = date_time_m1("2025-02-17 16:30:00","2025-02-16 16:30:00");
-        $dayM1_4 = date_time_m1("2025-02-18 16:30:00","2025-02-17 16:30:00");
-        $dayM1_5 = date_time_m1("2025-02-19 16:30:00","2025-02-18 16:30:00");
+        $dayM1_1 = date_time_m1("2025-03-20 16:30:00","2025-03-19 16:30:00");
+        $dayM1_2 = date_time_m1("2025-03-21 16:30:00","2025-03-20 16:30:00");
+        $dayM1_3 = date_time_m1("2025-03-22 16:30:00","2025-03-21 16:30:00");
+        $dayM1_4 = date_time_m1("2025-03-23 16:30:00","2025-03-22 16:30:00");
+        $dayM1_5 = date_time_m1("2025-03-24 16:30:00","2025-03-23 16:30:00");
         
         function date_time_m4($date1,$date2){
           include 'dblink.php';
           $value = mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(*) FROM regis_m4 WHERE date_regis <= '$date1' AND date_regis > '$date2' "));
           return $value['COUNT(*)'];
         }
-        $dayM4_1 = date_time_m4("2025-02-15 16:30:00","2025-02-13 16:30:00");
-        $dayM4_2 = date_time_m4("2025-02-16 16:30:00","2025-02-15 16:30:00");
-        $dayM4_3 = date_time_m4("2025-02-17 16:30:00","2025-02-16 16:30:00");
-        $dayM4_4 = date_time_m4("2025-02-18 16:30:00","2025-02-17 16:30:00");
-        $dayM4_5 = date_time_m4("2025-02-19 16:30:00","2025-02-18 16:30:00");
+        $dayM4_1 = date_time_m4("2025-03-20 16:30:00","2025-03-19 16:30:00");
+        $dayM4_2 = date_time_m4("2025-03-21 16:30:00","2025-03-20 16:30:00");
+        $dayM4_3 = date_time_m4("2025-03-22 16:30:00","2025-03-21 16:30:00");
+        $dayM4_4 = date_time_m4("2025-03-23 16:30:00","2025-03-22 16:30:00");
+        $dayM4_5 = date_time_m4("2025-03-24 16:30:00","2025-03-23 16:30:00");
       ?>
 
     <script type="text/javascript">
@@ -180,20 +225,20 @@
         type: 'bar',
         data: {
           labels: [
-            '15 ก.พ. 68', 
-            '16 ก.พ. 68', 
-            '17 ก.พ. 68', 
-            '18 ก.พ. 68', 
-            '19 ก.พ. 68'
+            '20 มี.ค. 68', 
+            '21 มี.ค. 68', 
+            '22 มี.ค. 68', 
+            '23 มี.ค. 68', 
+            '24 มี.ค. 68'
           ],
           datasets: [{
-            label: 'ห้องเรียนพิเศษ ม.1',
+            label: 'มัธยมศึกษาปีที่ 1',
             data: [<?=$dayM1_1?>, <?=$dayM1_2?>, <?=$dayM1_3?>, <?=$dayM1_4?>, <?=$dayM1_5?>],
             borderWidth: 1,
             borderColor: 'rgb(235, 54, 196)',
             backgroundColor: 'rgba(235, 54, 160, 0.2)'
           },{
-            label: 'ห้องเรียนพิเศษ ม.4',
+            label: 'มัธยมศึกษาปีที่ 4',
             data : [<?=$dayM4_1?>, <?=$dayM4_2?>, <?=$dayM4_3?>, <?=$dayM4_4?>, <?=$dayM4_5?>],
             borderWidth: 1,
             borderColor: 'rgb(54, 162, 235)',
@@ -214,9 +259,9 @@
       },
       });
     
-      $(window).on('load', function() {
-          $('#exampleModal').modal('show');
-      });
+      // $(window).on('load', function() {
+      //     $('#exampleModal').modal('show');
+      // });
 
       function myFunction1(){
         document.getElementById("openModalM1").setAttribute("data-bs-target","#ModalClose");
@@ -226,11 +271,18 @@
         document.getElementById("openModalM1").setAttribute("data-bs-target","#staticBackdropM1");
         document.getElementById("openModalM4").setAttribute("data-bs-target","#staticBackdropM4");
       }
+      function myFunction3(){
+        document.getElementById("openModalM1").setAttribute("data-bs-target","#ModalOpen");
+        document.getElementById("openModalM4").setAttribute("data-bs-target","#ModalOpen");
+      }
       
-      let a = new Date("2025-02-19T09:30:00Z")
+      let a = new Date("2025-03-24T09:30:00Z")
       let b = new Date()
-      if(b > a){
-        myFunction1();
+      let c = new Date("2025-03-19T02:30:00Z")
+      if(b < c){
+        myFunction3();
+      }else if(b > a){
+        myFunction1()
       }else{
         myFunction2();
       }
